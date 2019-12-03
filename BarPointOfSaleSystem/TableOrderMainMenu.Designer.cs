22e93c20f2af4d045cb9148b01d1745d6c7a42b3
Anastasia710 <31499133+Anastasia710@users.noreply.github.com>
2019-12-02 21:46:02 -0700

    Add buttons to the table menu
    Added buttons for add Customer, Add Order and Print Bill

--- BarPointOfSaleSystem/TableOrderMainMenu.Designer.cs
+++ BarPointOfSaleSystem/TableOrderMainMenu.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.TableNumberLabel = new System.Windows.Forms.Label();
+            this.AddCustBTN = new System.Windows.Forms.Button();
+            this.AddOrderBTN = new System.Windows.Forms.Button();
+            this.PrintBillBTN = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // TableNumberLabel
@@ -41,10 +44,40 @@
             this.TableNumberLabel.TabIndex = 20;
             this.TableNumberLabel.Text = "Table Number";
             // 
+            // AddCustBTN
+            // 
+            this.AddCustBTN.Location = new System.Drawing.Point(59, 68);
+            this.AddCustBTN.Name = "AddCustBTN";
+            this.AddCustBTN.Size = new System.Drawing.Size(106, 23);
+            this.AddCustBTN.TabIndex = 21;
+            this.AddCustBTN.Text = "Add Customer";
+            this.AddCustBTN.UseVisualStyleBackColor = true;
+            // 
+            // AddOrderBTN
+            // 
+            this.AddOrderBTN.Location = new System.Drawing.Point(59, 109);
+            this.AddOrderBTN.Name = "AddOrderBTN";
+            this.AddOrderBTN.Size = new System.Drawing.Size(106, 23);
+            this.AddOrderBTN.TabIndex = 22;
+            this.AddOrderBTN.Text = "Add Order";
+            this.AddOrderBTN.UseVisualStyleBackColor = true;
+            // 
+            // PrintBillBTN
+            // 
+            this.PrintBillBTN.Location = new System.Drawing.Point(59, 157);
+            this.PrintBillBTN.Name = "PrintBillBTN";
+            this.PrintBillBTN.Size = new System.Drawing.Size(106, 23);
+            this.PrintBillBTN.TabIndex = 23;
+            this.PrintBillBTN.Text = "Print Bill";
+            this.PrintBillBTN.UseVisualStyleBackColor = true;
+            // 
             // TableOrderMainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.PrintBillBTN);
+            this.Controls.Add(this.AddOrderBTN);
+            this.Controls.Add(this.AddCustBTN);
             this.Controls.Add(this.TableNumberLabel);
             this.Name = "TableOrderMainMenu";
             this.Size = new System.Drawing.Size(243, 235);
@@ -55,5 +88,8 @@
 
         #endregion
         private System.Windows.Forms.Label TableNumberLabel;
+        private System.Windows.Forms.Button AddCustBTN;
+        private System.Windows.Forms.Button AddOrderBTN;
+        private System.Windows.Forms.Button PrintBillBTN;
     }
 }
